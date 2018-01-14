29d701028a805259c56e0d710de0735c7b741561
Pavel Grinevich <grinevich.ps@gmail.com>
2018-01-14 14:08:31 +0300

    Implement methods 'getState()', 'changeState()', 'trigger()'

--- src/fsm.js
+++ src/fsm.js
@@ -3,25 +3,39 @@ class FSM {
      * Creates new FSM instance.
      * @param config
      */
-    constructor(config) {}
+    constructor(config) {
+        this.config = config;
+
+        this.state = this.config.initial;
+    }
 
     /**
      * Returns active state.
      * @returns {String}
      */
-    getState() {}
+    getState() {
+        return this.state;
+    }
 
     /**
      * Goes to specified state.
      * @param state
      */
-    changeState(state) {}
+    changeState(state) {
+        if (!this.config.states[state]) throw new Error('the state is not set');
+
+        this.state = state;
+    }
 
     /**
      * Changes state according to event transition rules.
      * @param event
      */
-    trigger(event) {}
+    trigger(event) {
+        if (!this.config.states[this.getState()].transitions[event]) throw new Error('the state is not set');
+
+        this.state = this.config.states[this.getState()].transitions[event];
+    }
 
     /**
      * Resets FSM state to initial.
@@ -59,3 +73,39 @@ class FSM {
 module.exports = FSM;
 
 /** @Created by Uladzimir Halushka **/
+
+const config = {
+    initial: 'normal',
+    states: {
+        normal: {
+            transitions: {
+                study: 'busy',
+            }
+        },
+        busy: {
+            transitions: {
+                get_tired: 'sleeping',
+                get_hungry: 'hungry',
+            }
+        },
+        hungry: {
+            transitions: {
+                eat: 'normal'
+            },
+        },
+        sleeping: {
+            transitions: {
+                get_hungry: 'hungry',
+                get_up: 'normal',
+            },
+        },
+    }
+};
+
+/*const student = new FSM(config);
+
+//console.log(student.getState());
+
+console.log(student.changeState('11hungry'));
+
+//console.log(student.getState());*/
